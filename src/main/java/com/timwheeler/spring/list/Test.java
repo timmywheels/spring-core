8c595f57a71cbea3a4e85c6109201af3b69296cf
Timothy Wheeler <timothywheeler@Timothys-iMac.local>
2019-06-01 19:38:41 -0400

    initial commit

---
+++ src/main/java/com/timwheeler/spring/list/Test.java
@@ -0,0 +1,13 @@
+package com.timwheeler.spring.list;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("listConfig.xml");
+        Hospital hospital = (Hospital) context.getBean("hospital");
+        System.out.println(hospital.getName());
+        System.out.println(hospital.getDepartments());
+    }
+}
